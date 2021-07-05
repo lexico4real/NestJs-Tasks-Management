1a96240277d0b0e09ae0be55927515fba269c545
lexico4real <lexico4real@gmail.com>
2021-07-05 12:29:20 +0100

    Creating a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,4 +11,17 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+    // console.log('Title: ', title);
+    // console.log('Description: ', description);
+  }
+  // createTask(@Body() body) {
+  //   console.log('Body: ', body);
+  // }
 }
